6f43499b04e927d92af876140ed4bc22d17f1bc1
Franklyn R <franklyn1998@live.com>
2022-08-29 19:19:41 -0300

    Adicionando o css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from "react-icons/fi";
+import "./styles.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <h1 className="title">Buscador CEP</h1>
+            <div className="containerInput">
+                <input type="text" placeholder="Digite seu CEP..." />
+                <button className="buttonSearch">
+                    <FiSearch size={25} color="#FFF" />
+                </button>
+            </div>
+            <main className="main">
+                <h2>CEP: 57308750</h2>
+                <span>Rua Teste alguma</span>
+                <span>Complemento: Alguma</span>
+                <span>Arapiraca</span>
+                <span>Alagoas - AL</span>
+            </main>
+        </div>
+    );
 }
 
 export default App;
